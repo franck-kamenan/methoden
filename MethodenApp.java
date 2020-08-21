fcb2677fa019ba8c1291d8908bb9c7966040f0bc
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-21 15:44:39 +0200

    Methoden

---
+++ MethodenApp.java
@@ -0,0 +1,11 @@
+package be.intecbrussel.hoofdstuk8.methoden;
+
+public class MethodenApp
+{
+    public static void main(String[] args)
+    {
+        MyMethodenClass methodMan = new MyMethodenClass();
+        System.out.println(methodMan.nr);
+        methodMan.uneMethode();
+    }
+}
---
+++ MyMethodenClass.java
@@ -0,0 +1,14 @@
+package be.intecbrussel.hoofdstuk8.methoden;
+
+public class MyMethodenClass
+{
+    public int nr = 5;
+
+    public void uneMethode()
+    {
+        int nr = 6;
+        System.out.println(nr);
+        System.out.println(this.nr);
+    }
+
+}
